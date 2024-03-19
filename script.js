f45eee83d2bc01f54a683e165d683a60df50d1ca
Wagner <wagner.worst@gmail.com>
2024-03-18 22:01:12 -0300

    Adiciona Lógica do JS e ajusta CSS do resultado

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 async function Cadastrar(){
     let tituloLivroInput = document.getElementById("formulario__titulo");
     let descricaoLivroInput = document.getElementById("formulario__descricao");
-    let formularioResultado = document.getElementById("formulario_resultado");
+    let formularioResultadoSucesso = document.getElementById("formularioResultado_sucesso");
+    let formularioResultadoErro = document.getElementById("formularioResultado_erro");
 
     let tituloLivro = tituloLivroInput.value;
     let descricaoLivro = descricaoLivroInput.value;
 
     if (!tituloLivro || !descricaoLivro)
     {
-        formularioResultado.innerText="Formulário Inválido.";
+        formularioResultadoSucesso.style.display = "none"
+        formularioResultadoErro.style.display = "block"
     }
     else
     {
         const url = "https://api-aula.up.railway.app/livros";
-        const retorno = await cadastrarLivroAPI(url,)
-        formularioResultado.innerText="Enviado com Sucesso!";
+        const payload = {
+            title: tituloLivro,
+            description: descricaoLivro
+        }
+        //opções de como o fetch vai ser feito
+        const fetchOptions = {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {"Content-type": "application/json"}
+        }
+        //manda o fetch executar mandando a URL e suas opções
+        fetch(url,fetchOptions);
+        //altera o css da div de sucesso e esconde a de erro
+        formularioResultadoErro.style.display = "none"
+        formularioResultadoSucesso.style.display = "block"
     }
 }
 
-async function cadastrarLivroAPI(url, body){
+/*async function cadastrarLivroAPI(url, body){
     const respostaApi = await fetch(url,{
         method: "POST",
         body: JSON.stringfy(body),
@@ -27,4 +42,4 @@ async function cadastrarLivroAPI(url, body){
 
     const respostaApiJson = await respostaApi.json();
     return respostaApiJson;
-}
+}*/
